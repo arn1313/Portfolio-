1448ec2f419a02a3261001969db9ae5eb7603a6d
Aaron McPeek <arn1313@comcast.net>
2017-07-27 11:36:42 -0700

    adding in heroku

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 'use strict';
 
+const pg = ('pg)');
+const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-const bodyParser = require('body-parser').urlencoded({extended: true});
-const PORT = process.env.PORT || 3000;
+const conString = process.env.DATABASE_URL;
 
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./'));
 
-app.get('/new', function(request, response){
-  console.log('request sent');
-  response.sendFile('/index.html', {root: './'});
-});
-
-app.post('./articles', bodyParser, function(request, response){
-  console.log(request.body);
-  response.send('Record Posted');
-});
-
-app.listen(PORT, function(){
-  console.log(`Listen to port: ${PORT}`);
-})
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for',
+  request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com${request.params[0]}`,
+    headers: {Authorization: `token${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+app.get('github/*', proxyGitHub);
